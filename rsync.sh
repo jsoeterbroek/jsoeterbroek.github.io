0e91e93b2a491815e9825d85086501fceade2f15
joost soeterbroek <joost.soeterbroek@gmail.com>
2020-09-02 13:45:32 +0200

    minor

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,4 @@
 rsync -auv /var/tmp/freeze/ .
 rsync -auv ../friendly-octo-waffle/datastore/ datastore/
+rsync -auv ../friendly-octo-waffle/stats/static/* static/
+#rsync -auv ../friendly-octo-waffle/keys/static/* static/
